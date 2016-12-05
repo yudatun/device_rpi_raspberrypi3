6b773e95cd25358bef5d57ceba72abce2e5b48fd
liminghao <liminghao@xiaomi.com>
2016-12-05 16:58:23 +0800

    packages.mk: remove to bluetooth firmewares.
    N/A
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

--- packages.mk
+++ packages.mk
@@ -3,8 +3,3 @@ include device/rpi/raspberrypi-common/packages.mk
 
 ########################################
 include vendor/raspberrypi/firmware/packages.mk
-
-########################################
-PRODUCT_PACKAGES += \
-    bt_vendor.conf \
-    BCM43430A1.hcd \
